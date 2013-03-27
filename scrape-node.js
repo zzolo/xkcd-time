01c81f594783c8cb5139edaf0b05ddb399a2b3c2
Alan Palazzolo <apalazzolo@minnpost.com>
2013-03-27 14:54:31 -0500

    Updating scraper a bit and adding in new images.

--- scrape-node.js
+++ scrape-node.js
@@ -7,34 +7,40 @@ var http = require('http');
 var gag = process.argv[2];
 var url = 'http://www.explainxkcd.com/wiki/index.php?title=' + gag;
 
+// Make directory if needed
 fs.mkdir('images', function(e) {});
 
+// Get page
 request(url, function(err, resp, body) {
-
   if (resp.statusCode !== 200) {
     console.log('Fail to get the page: ' + url);
     return;
   }
 
-  fs.mkdir('images/', function(e) {});
-
+  // Parse HTML with cheerio
   $ = cheerio.load(body);
+  
+  // Go through links
   var links = $('#mw-content-text dl a');
-
   $(links).each(function(i, link) {
     var href = $(link).attr('href');
 
     if (href.indexOf('images') !== -1 && href.indexOf('/time') !== -1 && href.indexOf('.png') !== -1) {
+      // Pad zeroes to keep order
       var match = href.match(/time([0-9]*).png/i);
       var number = match[1];
       while (number.length < 5) number = "0" + number;
 
+      // Get each file
       var file = fs.createWriteStream('images/' + number + '.png');
       var request = http.get('http://www.explainxkcd.com' + href, function(response) {
-        response.pipe(file);
+        if (resp.statusCode == 200) {
+          response.pipe(file);
+        }
+        else {
+          console.log('Fail to get the page: ' + 'http://www.explainxkcd.com' + href);
+        }
       });
-
     }
-
   });
 });
