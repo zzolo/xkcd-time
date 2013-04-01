d7107ea6d5fb169ab7853ad1ec90804669d548d9
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-01 17:10:42 -0500

    Updating script to handle scraping better and updating images.

--- scrape-node.js
+++ scrape-node.js
@@ -21,19 +21,29 @@ request(url, function(err, resp, body) {
   $ = cheerio.load(body);
   
   // Go through links
-  var links = $('#mw-content-text dl a');
+  var links = $('#mw-content-text a');
   $(links).each(function(i, link) {
     var href = $(link).attr('href');
 
     if (href.indexOf('images') !== -1 && href.indexOf('/time') !== -1 && href.indexOf('.png') !== -1) {
       // Pad zeroes to keep order
-      var match = href.match(/time([0-9]*).png/i);
+      var match = href.match(/time([0-9]*)([A-z]*).png/i);
       var number = match[1];
       while (number.length < 5) number = "0" + number;
+      // There are a few that have a suffix.  Not sure what
+      // to do with that yet.
+      if (match[2]) {
+        return;
+      }
+      
+      // Check if full URL
+      if (href.indexOf('http') !== 0) {
+        href = 'http://www.explainxkcd.com' + href;
+      }
 
       // Get each file
       var file = fs.createWriteStream('images/' + number + '.png');
-      var request = http.get('http://www.explainxkcd.com' + href, function(response) {
+      var request = http.get(href, function(response) {
         if (resp.statusCode == 200) {
           response.pipe(file);
         }
