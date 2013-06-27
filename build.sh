e7cfb8680591f663bd1a52b4640808477327bc92
Alan Palazzolo <apalazzolo@minnpost.com>
2013-06-27 08:40:23 -0500

    Update.

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
-node scrape-node.js 1190 && \
-convert -delay 80 -loop 0 images/*.png xkcd-time.gif && \
-convert -delay 20 -loop 0 images/*.png xkcd-time-fast.gif && \
-convert -delay 7 -loop 0 images/*.png xkcd-time-super-fast.gif && \
-convert -delay 2 -loop 0 images/*.png xkcd-time-ludicrous-speed.gif;
+#set NODE_PATH=`which node`
+set CONVERT_PATH=`which convert`
+
+NODE_PATH=/usr/local/bin/node
+CONVERT_PATH=/usr/local/bin/convert
+
+echo $NODE_PATH
+echo $CONVERT_PATH
+
+$NODE_PATH scrape-node.js 1190 && \
+$CONVERT_PATH -delay 80 -loop 0 images/*.png xkcd-time.gif && \
+$CONVERT_PATH -delay 20 -loop 0 images/*.png xkcd-time-fast.gif && \
+$CONVERT_PATH -delay 7 -loop 0 images/*.png xkcd-time-super-fast.gif && \
+$CONVERT_PATH -delay 2 -loop 0 images/*.png xkcd-time-ludicrous-speed.gif;
